37226819aa3682c6ed5d05a651fd49440b03ae33
Yosof Badr <yosof_badr@hotmail.com>
2020-11-30 21:24:25 +0000

    Set up frame rate cap.

--- StrangeAttractors/StrangeAttractors.cpp
+++ StrangeAttractors/StrangeAttractors.cpp
@@ -18,12 +18,20 @@ int main(int argc, char* args[])
                             Vertex(glm::vec3(0.01, -0.01, 0)),
                         };
 
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+
+    Uint32 frameStart;
+    int frameTime;
+
     Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
     Shader shader("./res/shader");
     Transform transform;
 
     // While window is running do some drawing
     while (!display.IsClosed()) {
+        frameStart = SDL_GetTicks();
+
         display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
 
         shader.Bind();
@@ -31,6 +39,12 @@ int main(int argc, char* args[])
         mesh.Draw();
 
         display.Update();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if (frameDelay > frameTime) {
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
 
     return 0;
