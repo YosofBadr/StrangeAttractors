9c9acaa3b35c25c87a789b3ca083d2cdbc0eaa5d
Yosof Badr <yosof_badr@hotmail.com>
2020-12-02 23:18:10 +0000

    Set up a particle class.

--- StrangeAttractors/StrangeAttractors.cpp
+++ StrangeAttractors/StrangeAttractors.cpp
@@ -4,9 +4,18 @@
 #include "shader.h"
 #include "mesh.h"
 #include "transform.h"
+#include "particle.h"
 
 #define WIDTH 1920
 #define HEIGHT 1080
+
+
+float myRandom()
+//Return random double within range [0,1]
+{
+    return (rand() / (float)RAND_MAX);
+}
+
 
 int main(int argc, char* args[])
 {
@@ -23,7 +32,8 @@ int main(int argc, char* args[])
 //                    Vertex(glm::vec3(-0.25, -0.5, 0)),
 //                    Vertex(glm::vec3(-0.75, -0.5, 0)),
 //};
-    const int FPS = 30;
+    glEnable(GL_POINT_SMOOTH);
+    const int FPS = 60;
     const int frameDelay = 1000 / FPS;
 
     Uint32 frameStart;
@@ -33,17 +43,24 @@ int main(int argc, char* args[])
     Shader shader("./res/shader");
     Transform transform;
 
+    const unsigned int numOfParticles = 10000;
+    Particle particles[numOfParticles];
+
+    // Init particles
+    for (int i = 0; i < numOfParticles; i++) { 
+        float xCoord = myRandom();
+        float yCoord = myRandom();
+        float zCoord = myRandom();
+        particles[i] = Particle(glm::vec3(xCoord, yCoord, zCoord), glm::vec4(1.0, 1.0, 0.0, 1.0));
+    }
+
     // Change in time
-    float dt = 0.005f;
+    float dt = 0.001f;
 
     // Constants/World Variables, change the outcome of the system
     float sigma = 10;
     float rho = 28;
     float beta = 8.0 / 3.0;
-
-    float x = 1;
-    float y = 1;
-    float z = 1;
 
     float scale = 0.02f;
 
@@ -53,9 +70,11 @@ int main(int argc, char* args[])
 
         display.Clear(0.0f, 0.0f, 0.0f, 0.0f);
 
-        // Follows path of lerenz attractor 
-        glPointSize(5);
-        glBegin(GL_POINTS);
+        for (int i = 0; i < numOfParticles; i++) {
+            float x = particles[i].GetPos().x;
+            float y = particles[i].GetPos().y;
+            float z = particles[i].GetPos().z;
+
             // Compute change in position based on current position 
             float dx = (sigma * (y - x)) * dt;
             float dy = ((x * (rho - z)) - y) * dt;
@@ -66,8 +85,16 @@ int main(int argc, char* args[])
             y += dy;
             z += dz;
 
-            // Draw point
-            glVertex3f(x * scale, y * scale, z * scale);
+            particles[i].SetPos(glm::vec3(x, y, z));
+        }
+
+        // Follows path of lerenz attractor 
+        glPointSize(5);
+        glBegin(GL_POINTS);
+            for (int i = 0; i < numOfParticles; i++) {
+                // Draw point
+                glVertex3f(particles[i].GetPos().x * scale, particles[i].GetPos().y * scale, particles[i].GetPos().z * scale);
+            }
         glEnd();
 
         shader.Bind();
