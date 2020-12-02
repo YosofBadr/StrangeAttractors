8a37f6499b8dd6960f76a0b85ab29890500a7ff6
Yosof Badr <yosof_badr@hotmail.com>
2020-12-02 21:23:32 +0000

    Set up single point following lorenz attractor.

--- StrangeAttractors/StrangeAttractors.cpp
+++ StrangeAttractors/StrangeAttractors.cpp
@@ -12,31 +12,67 @@ int main(int argc, char* args[])
 {
     Display display(WIDTH, HEIGHT, "Strange Attractor");
 
-    Vertex vertices[] = {   
-                            Vertex(glm::vec3(-0.01, -0.01, 0.0)), 
-                            Vertex(glm::vec3(0, 0.01, 0)),
-                            Vertex(glm::vec3(0.01, -0.01, 0)),
-                        };
+    //Vertex vertices[] = {   
+    //                        Vertex(glm::vec3(-0.5, -0.5, 0.0)), 
+    //                        Vertex(glm::vec3(0, 0.5, 0)),
+    //                        Vertex(glm::vec3(0.5, -0.5, 0)),
+    //                    };
 
-    const int FPS = 60;
+    //Vertex vertices[] = {
+//                    Vertex(glm::vec3(-0.5, 0.5, 0.0)),
+//                    Vertex(glm::vec3(-0.25, -0.5, 0)),
+//                    Vertex(glm::vec3(-0.75, -0.5, 0)),
+//};
+    const int FPS = 30;
     const int frameDelay = 1000 / FPS;
 
     Uint32 frameStart;
     int frameTime;
 
-    Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+    //Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
     Shader shader("./res/shader");
     Transform transform;
+
+    // Change in time
+    float dt = 0.005f;
+
+    // Constants/World Variables, change the outcome of the system
+    float sigma = 10;
+    float rho = 28;
+    float beta = 8.0 / 3.0;
+
+    float x = 1;
+    float y = 1;
+    float z = 1;
+
+    float scale = 0.02f;
 
     // While window is running do some drawing
     while (!display.IsClosed()) {
         frameStart = SDL_GetTicks();
 
-        display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
+        display.Clear(0.0f, 0.0f, 0.0f, 0.0f);
+
+        // Follows path of lerenz attractor 
+        glPointSize(5);
+        glBegin(GL_POINTS);
+            // Compute change in position based on current position 
+            float dx = (sigma * (y - x)) * dt;
+            float dy = ((x * (rho - z)) - y) * dt;
+            float dz = ((x * y) - (beta * z)) * dt;
+
+            // Update location
+            x += dx;
+            y += dy;
+            z += dz;
+
+            // Draw point
+            glVertex3f(x * scale, y * scale, z * scale);
+        glEnd();
 
         shader.Bind();
         shader.Update(transform);
-        mesh.Draw();
+        //mesh.Draw();
 
         display.Update();
 
